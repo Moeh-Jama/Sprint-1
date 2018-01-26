91c4ccba5d0246a2977885d0b03db8a7209495f3
jingyuantan <32653129+jingyuantan@users.noreply.github.com>
2018-01-26 12:40:48 +0000

    contorl panel

--- src/board/Board.java
+++ src/board/Board.java
@@ -1,3 +1,4 @@
+package board;
 import java.awt.Color;
 import java.awt.Graphics;
 
@@ -7,20 +8,22 @@ public class Board extends JFrame{
 	public static void main (String[] args)
 	{
 		new Board();
+		Control controlPanel = new Control();
+		controlPanel.panel();
 		
 	}
 	
 	public Board() {
 		this.setTitle("Cluedo Board");
 		this.setSize(640, 638);
-		paint(10,10,30,30);
+		//paint(10,10,30,30);
 		displayImage();
 		this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
 		this.setVisible(true);
 	}
 	
 	private void displayImage() {
-		ImageIcon icon = new ImageIcon(".\\CluedBoard.jpg");
+		ImageIcon icon = new ImageIcon("CluedBoard.jpg");
 		this.add(new JLabel(icon));
 	}
 	public  void paint(int x,int y,int XSIZE,int YSIZE) {
