036bab938eec4940d1ae2dadc0e06368c93d418d
jingyuantan <32653129+jingyuantan@users.noreply.github.com>
2018-01-28 16:12:18 +0000

    commit merge

--- src/board/Board.java
+++ src/board/Board.java
@@ -1,4 +1,5 @@
 package board;
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Graphics;
 
@@ -17,9 +18,37 @@ public class Board extends JFrame{
 		this.setTitle("Cluedo Board");
 		this.setSize(640, 638);
 		//paint(10,10,30,30);
+=======
+	private int Height = 640;
+	private int Width = 638;
+	
+	
+	public Board() {
+		this.setTitle("Cluedo Board");
+		this.setSize(Height, Width);
+		//paint(10,10,30,30);
+		makeBorder();
+>>>>>>> master:Board.java
 		displayImage();
+		
+
+		
+		
 		this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
 		this.setVisible(true);
+	}
+	private void makeBorder() {
+		JPanel panel = new JPanel();
+		panel.setSize((int) (Width * 0.87), (int)(Height * 0.898));
+		//panel.setBorder(BorderFactory.createEmptyBorder(0,0,0,0));
+		
+		//Making the background set.
+		panel.setOpaque(false);
+		//JLabel x = new JLabel("Hello World");
+		panel.setLocation((int)30, (int)3);
+		//panel.add(x);
+		System.out.println(panel.getSize());
+		this.add(panel);//, BorderLayout.CENTER);
 	}
 	private void displayImage() {
 		ImageIcon icon = new ImageIcon("CluedBoard.jpg");
