d156fd2a000fbbf4a4d014998f12bdee2b42fb2f
rahul praveen <rahulpraveen16@gmail.com>
2018-02-11 16:59:41 +0000

    commented and indented players,, minor changes in board

--- src/Board.java
+++ src/Board.java
@@ -22,238 +22,122 @@ import java.util.Scanner;
 
 import javax.swing.*;
 
-
-public class Board extends JPanel{
+public class Board extends JPanel {
 	private int Height = 640;
 	private int Width = 638;
-	private PlayerPanel panel = new PlayerPanel();
-	
-	private PlayerPanel[] currentPlayers = new PlayerPanel[5];
+	private player panel = new player();
+
+	private player[] currentPlayers = new player[6];
 	private JPanel GameBoardPanel = new JPanel();
-	//private Panel panel = new Panel();
+	// private Panel panel = new Panel();
 	private JLabel backgroundImage;
-	
-	class BigPanel extends JPanel{
+
+	class BigPanel extends JPanel {
 		BufferedImage image;
-		
+
 		Image bg = new ImageIcon("src/image/CluedBoard.jpg").getImage();
-	    @Override
-	    public void paintComponent(Graphics g) {
-	    	super.paintComponent(g);
-	        g.drawImage(bg, 0, 0,getWidth()-1, getHeight()-1, this);
-	    }
-	}
-	
-	class PlayerPanel extends JPanel{
-		private double x=0;
-		private double y = 0;
-		private int volX=23*4;
-		private int volY=23*4;
-		Color player = Color.RED;
-		
-		//22.5*11
+
+		@Override
 		public void paintComponent(Graphics g) {
-			System.out.println(x+", "+y);
-			/*super.paintComponent(g);
-			
-			Graphics2D g2 = (Graphics2D) g;
-			
-			Ellipse2D c = new Ellipse2D.Double(x,y, 23, 23);
-			g2.setColor(player);
-			g2.fill(c);
-			*/
-			
-			
-			Image bg = new ImageIcon("src/image/uganda_k.png").getImage();
-		    //@Override
-		    
-		    	super.paintComponent(g);
-		        g.drawImage(bg, (int)x, (int)y, 23*4,23*4, this);
-		        
-			
-			
-		}
-		
-		public void setPX(double x) { this.x = x;}
-		public double getPlayerX() {return x;}
-		
-		public void setPY(double y) {this.y = y;}
-		public double getPlayerY() {return y;}
-		
-		public void setPlayer(String playerName) {
-			switch(playerName) {
-			case "Example1":{
-				player = Color.RED;
-			}
-			case "Example2":{
-				player = Color.BLUE;
-			}
-			default:{
-				System.out.println("Player could not be assigned Color: setPlayerName");
-				System.exit(0);
-			}
-			}
-		}
-		
-		public void move(String movementType) {
-			switch(movementType) {
-			case "U":{
-				if(y>0)
-					y -= volY;
-				break;
-				
-			}
-			case "D":{
-				if(y<23*24)
-					y += volY;
-				break;
-			}
-			case "R":{
-				if(x<23*23)
-					x += volX;
-				break;
-			}
-			case "L":{
-				if(x>0)
-					x -= volX;
-				break;
-			}
-			}
-			repaint();
+			super.paintComponent(g);
+			g.drawImage(bg, 0, 0, getWidth() - 1, getHeight() - 1, this);
 		}
 	}
-	
+
 	JPanel bacgroundPanel = new BigPanel();
+
 	public Board() {
-		//drawAllPlayersOnBoard();
-		createGridPanel();
+		drawAllPlayersOnBoard();
 		this.setSize(Height, Width);
-		//this.setSize(Width, Height);
-		
+		// this.setSize(Width, Height);
+
 		bacgroundPanel.setLayout(null);
-		//bacgroundPanel.setSize(Width, Height);
-		//createGridPanel();
-		
-		bacgroundPanel.add(panel);
-		bacgroundPanel.setPreferredSize(new Dimension(Height,Width));
+		// bacgroundPanel.setSize(Width, Height);
+		// createGridPanel();
+
+		bacgroundPanel.setPreferredSize(new Dimension(Height, Width));
 
 		this.add(bacgroundPanel);
 
-		//displayImage();
-		
+		// displayImage();
 
-		
 	}
+
 	public void resizeGameBoard() {
-		GameBoardPanel.setSize(50,50);
+		GameBoardPanel.setSize(50, 50);
 	}
-	
+
 	public void drawAllPlayersOnBoard() {
-		
-		String[] starting_pos = { "0, 9", "0, 14","6, 23","19, 23","24, 7","17, 0"};
-		//BLACK
-		//RED
-		//GREEN
-		//ORANGE
-		//YELLOW
 
+		String[] starting_pos = { "0, 9", "0, 14", "6, 23", "19, 23", "24, 7", "17, 0" };
+		// BLACK
+		// RED
+		// GREEN
+		// ORANGE
+		// YELLOW
 
-		for(int i=0; i<currentPlayers.length; i++)
-		{
+		for (int i = 0; i < currentPlayers.length; i++) {
 			int x = Integer.parseInt(starting_pos[i].split(", ")[0]);
 			int y = Integer.parseInt(starting_pos[i].split(", ")[1]);
-//			System.out.println);
-			System.out.println("Positions of Players"+": "+x*23+", "+y*23);
-			currentPlayers[i] = new PlayerPanel();
-			currentPlayers[i].setPX(x*23);
-			currentPlayers[i].setPY(y*24);
+			// System.out.println);
+			System.out.println("Positions of Players" + ": " + x * 23 + ", " + y * 23);
+			currentPlayers[i] = new player();
+			currentPlayers[i].setPositionX(x * 23);
+			currentPlayers[i].setPositionY(y * 24);
 			playerPanelGrid(currentPlayers[i]);
 			bacgroundPanel.add(currentPlayers[i]);
 		}
 	}
-	
+
 	public void addButtonToPanel() {
 		JLabel msg = new JLabel("Hello, and welcome");
 		msg.setFont(new Font("Serif", Font.BOLD, 20));
 		msg.setForeground(Color.ORANGE);
-		
+
 		panel.add(msg);
-		//"0, 9", "0, 14","6, 23","19, 23","24, 7","17, 0"
+		// "0, 9", "0, 14","6, 23","19, 23","24, 7","17, 0"
 		panel.revalidate();
 	}
-	
-	
-	
-	
-	
+
 	public int getPanelHeight() {
 		return panel.getHeight();
 	}
+
 	public int getPanelWidth() {
 		return panel.getSize().width;
 	}
 
 	public void movePlayer(String res) {
 		panel.move(res);
-		//Random r = new Random();
-		//currentPlayers[r.nextInt(5)].move(res);
+		// Random r = new Random();
+		// currentPlayers[r.nextInt(5)].move(res);
 	}
-	private PlayerPanel playerPanelGrid(PlayerPanel panel) {
+
+	private player playerPanelGrid(player panel) {
 		/*
 		 * This function sets the size of t
 		 */
 		int panel_width = (int) (Width * 0.8655);
-		int panel_height =  (int)(Height * 0.898);
+		int panel_height = (int) (Height * 0.898);
 		Dimension panelSize = new Dimension(panel_width, panel_height);
 		panel.setPreferredSize(panelSize);
 		panel.setSize(panelSize);
-		//System.out.println("Panel size is: "+panel.getSize());
-		
-		
-		//Making the background set.
+		// System.out.println("Panel size is: "+panel.getSize());
+
+		// Making the background set.
 		panel.setOpaque(false);
-		//The panel is set to the middle of the board.
-		
-		
+		// The panel is set to the middle of the board.
 
 		int x = 42;
 		int y = 23;
 		panel.setLocation(x, y);
 
 		return panel;
-			
-	}
-	
-	private void createGridPanel() {
-		/*
-		 * This function sets the size of t
-		 */
-		int panel_width = (int) (Width * 0.8655);
-		int panel_height =  (int)(Height * 0.898);
-		Dimension panelSize = new Dimension(panel_width, panel_height);
-		panel.setPreferredSize(panelSize);
-		panel.setSize(panelSize);
-		//System.out.println("Panel size is: "+panel.getSize());
-		
-		
-		//Making the background set.
-		panel.setOpaque(false);
-		//The panel is set to the middle of the board.
-		
-		
 
-		int x = 42;
-		int y = 23;
-		panel.setLocation(x, y);
-
-			
-			
 	}
-	
 
 	/*
 	 * The Mouse Listener is no longer needed now.
 	 */
-	
-	
+
 }
--- src/player.java
+++ src/player.java
@@ -1,22 +1,113 @@
+<<<<<<< HEAD
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.util.*;
-public class player {
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+public class player extends JPanel {
+	private String name;//Name of the player	
+	private double player_X_CoOrdinate;//co-ordinate X of the player
+	private double player_Y_CoOrdinate;//Y co-ordinate
+	private int velocity_X = 23 * 4;
+	private int velocity_Y = 23 * 4;
+	Color player = Color.RED;
+
+	public void setPositionX(double x) {
+		this.player_X_CoOrdinate = x;
+	}
+//Sets Y-coOrdinate of the player
+	public void setPositionY(double y) {
+		this.player_Y_CoOrdinate = y;
+	}
+//Returns players name
+	public String getPlayerName() {
+=======
+
+public class Player {
 	private String name;
 	private int x;
 	private int y;
 	
-	public player(String name, int a,int b){
+	public Player(String name, int a,int b){
 		this.x=a;
 		this.y=b;
 		this.name=name;
 	}
 	
 	public String getPlayerName(){
+>>>>>>> 288fa74e8f60004bea748185eca092c09b71d250
 		return this.name;
 	}
-	public int getPlayerX(){
-		return this.x;
+//Function to return players X co-ordinate
+	public double getPlayerPostionX() {
+		return this.player_X_CoOrdinate;
 	}
-	public int getPlayerY(){
-		return this.y;
+//function to return players Y co-ordinate
+	public double getPlayerPositionY() {
+		return this.player_Y_CoOrdinate;
+	}
+//Function to move the players over the board
+	public void move(String movementType) {
+		switch (movementType) {
+		case "U": {
+			if (player_Y_CoOrdinate > 0)
+				player_Y_CoOrdinate -= velocity_Y;
+			break;
+
+		}
+		case "D": {
+			if (player_Y_CoOrdinate < 23 * 24)
+				player_Y_CoOrdinate += velocity_Y;
+			break;
+		}
+		case "R": {
+			if (player_X_CoOrdinate < 23 * 23)
+				player_X_CoOrdinate += velocity_X;
+			break;
+		}
+		case "L": {
+			if (player_X_CoOrdinate > 0)
+				player_X_CoOrdinate -= velocity_X;
+			break;
+		}
+		}
+		repaint();
+	}
+	//Assigning colours to players
+	public void setPlayer(String playerName) {
+		switch (playerName) {
+		case "Example1": {
+			player = Color.RED;
+		}
+		case "Example2": {
+			player = Color.BLUE;
+		}
+		default: {
+			System.out.println("Player could not be assigned Color: setPlayerName");
+			System.exit(0);
+		}
+		}
+	}
+
+	public void paintComponent(Graphics g) {
+		System.out.println(player_X_CoOrdinate + ", " + player_Y_CoOrdinate);
+		/*
+		 * super.paintComponent(g);
+		 * 
+		 * Graphics2D g2 = (Graphics2D) g;
+		 * 
+		 * Ellipse2D c = new Ellipse2D.Double(x,y, 23, 23); g2.setColor(player);
+		 * g2.fill(c);
+		 */
+
+		Image bg = new ImageIcon("src/image/uganda_k.png").getImage();
+		// @Override
+
+		super.paintComponent(g);
+		g.drawImage(bg, (int) player_X_CoOrdinate, (int) player_Y_CoOrdinate, 23 * 4, 23 * 4, this);
+
 	}
 }
